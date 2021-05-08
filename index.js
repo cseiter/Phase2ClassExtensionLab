e1f3710cc5d27799cd2d40f08a368dd56292e235
chris <cseiter@gmail.com>
2021-05-08 14:57:10 -0400

    Polygon class tests passed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+class Polygon {
+    constructor(arrSides) {
+        this.arrSides = arrSides;
+        this.count = this.arrSides.length;
+    }
+    get countSides() {
+        return this.arrSides.length;
+    }
+    get perimeter() {
+        return this.arrSides.reduce((a,b)=> a+b,0)
+    }
+}
